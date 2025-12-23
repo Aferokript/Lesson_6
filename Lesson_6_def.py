b360e914a5c4cf968984b3b4bf109e786699129e
Aferokript <alberto.cardinale@bk.ru>
2025-12-23 16:44:49 +0300

    Update Lesson_6_def.py

--- Lesson_6_def.py
+++ Lesson_6_def.py
@@ -1,9 +1,5 @@
 def is_very_long(password):
-    password_length = len(password)
-    if password_length > 12:
-        return True
-    else:
-        return False
+    return len(password) >= 12
 
 def has_digits(password):
     return any(c.isdigit() for c in password)
@@ -18,13 +14,32 @@ def has_lower_letters(password):
     return any(p.islower() for p in password)
 
 def has_symbols(password):
-    return any(p.isalnum() for p in password)
+    return any('@' or '$' or '#' or '%' or '^' or '&' or '*' or '!' or '(' or ')' in p for p in password)
 
 def rating_password(password):
-    check = [has_digits, has_letter, has_upper_letters, has_lower_letters, has_symbols]
+    check = [
+       has_digits,
+       has_letter,
+       has_upper_letters,
+       has_lower_letters,
+       has_symbols
+    ]
     score = 0
     for c in check:
         if c(password):
             score += 2
     rating = f'Рейтинг пароля {score}'
     return rating
+
+def main():
+    password = input('Введите пароль ')
+    print(is_very_long(password))
+    print(has_digits(password))
+    print(has_letter(password))
+    print(has_upper_letters(password))
+    print(has_lower_letters(password))
+    print(has_symbols(password))
+    print(rating_password(password))
+
+if __name__ == '__main__':
+    main()
