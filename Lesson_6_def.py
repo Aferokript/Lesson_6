8034c536c8c6d0cbde6c0283d99854a9a6580e41
Aferokript <alberto.cardinale@bk.ru>
2025-12-24 15:18:14 +0300

    Update Lesson_6_def.py

--- Lesson_6_def.py
+++ Lesson_6_def.py
@@ -1,20 +1,26 @@
 def is_very_long(password):
     return len(password) >= 12
 
+
 def has_digits(password):
     return any(c.isdigit() for c in password)
+
 
 def has_letter(password):
     return any(p.isalpha() for p in password)
 
+
 def has_upper_letters(password):
     return any(p.isupper() for p in password)
+
 
 def has_lower_letters(password):
     return any(p.islower() for p in password)
 
+
 def has_symbols(password):
     return any(not p.isalnum() for p in password)
+
 
 def rating_password(password):
     check = [
@@ -31,9 +37,11 @@ def rating_password(password):
             score += 2
     return f'Рейтинг пароля {score}'
 
+
 def main():
     password = input('Введите пароль ')
     print(rating_password(password))
 
 if __name__ == '__main__':
     main()
+
